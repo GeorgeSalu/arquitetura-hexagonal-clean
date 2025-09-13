e3724950d730601bba99d8c4f056cca41e407e77
George Salu <george.salu10@gmail.com>
2025-09-13 10:23:00 -0300

    clean-arch - create - criando o repository

---
+++ clean-arch/src/main/java/com/demo/clean_arch/dataprovider/repository/CustomerRepository.java
@@ -0,0 +1,9 @@
+package com.demo.clean_arch.dataprovider.repository;
+
+import com.demo.clean_arch.dataprovider.repository.entity.CustomerEntity;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CustomerRepository extends MongoRepository<CustomerEntity, String> {
+}
