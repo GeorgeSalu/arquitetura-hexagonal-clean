d306c88fe2c992dde36435a0805a6d546b8cb962
George Salu <george.salu10@gmail.com>
2025-09-14 10:40:34 -0300

    clean-arch - update - criando o use case

---
+++ clean-arch/src/main/java/com/demo/clean_arch/core/dataprovider/UpdateCustomer.java
@@ -0,0 +1,9 @@
+package com.demo.clean_arch.core.dataprovider;
+
+import com.demo.clean_arch.core.domain.Customer;
+
+public interface UpdateCustomer {
+
+    void update(Customer customer);
+
+}
---
+++ clean-arch/src/main/java/com/demo/clean_arch/core/usecase/impl/UpdateCustomerUseCaseImpl.java
@@ -0,0 +1,28 @@
+package com.demo.clean_arch.core.usecase.impl;
+
+import com.demo.clean_arch.core.dataprovider.FindAddressByZipCode;
+import com.demo.clean_arch.core.dataprovider.UpdateCustomer;
+import com.demo.clean_arch.core.domain.Customer;
+import com.demo.clean_arch.core.usecase.FindCustomerByIdUseCase;
+import com.demo.clean_arch.core.usecase.UpdateCustomerUseCase;
+
+public class UpdateCustomerUseCaseImpl implements UpdateCustomerUseCase {
+
+    private final FindCustomerByIdUseCase findCustomerByIdUseCase;
+    private final FindAddressByZipCode findAddressByZipCode;
+    private final UpdateCustomer updateCustomer;
+
+    public UpdateCustomerUseCaseImpl(FindCustomerByIdUseCase findCustomerByIdUseCase, FindAddressByZipCode findAddressByZipCode, UpdateCustomer updateCustomer) {
+        this.findCustomerByIdUseCase = findCustomerByIdUseCase;
+        this.findAddressByZipCode = findAddressByZipCode;
+        this.updateCustomer = updateCustomer;
+    }
+
+    @Override
+    public void update(Customer customer, String zipCode) {
+        findCustomerByIdUseCase.find(customer.getId());
+        var address = findAddressByZipCode.find(zipCode);
+        customer.setAddress(address);
+        updateCustomer.update(customer);
+    }
+}
