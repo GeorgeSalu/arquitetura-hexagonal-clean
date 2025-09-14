322d793c1cc09c6fc8f6ef0253e060ecdf49685d
George Salu <george.salu10@gmail.com>
2025-09-14 17:39:42 -0300

    criando o projeto hexagonal-arch

---
+++ hexagonal-arch/src/main/java/com/demo/hexagonal_arch/HexagonalArchApplication.java
@@ -0,0 +1,13 @@
+package com.demo.hexagonal_arch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HexagonalArchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HexagonalArchApplication.class, args);
+	}
+
+}
