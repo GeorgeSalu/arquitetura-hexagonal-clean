9a780b9ae00c1cef4a1af5cae40fd1b15e47575e
George Salu <george.salu10@gmail.com>
2025-09-14 12:28:23 -0300

    clean-arch - criando bean para FindCustomerById

--- clean-arch/src/main/java/com/demo/clean_arch/config/FindCustomerByIdConfig.java
+++ clean-arch/src/main/java/com/demo/clean_arch/config/FindCustomerByIdConfig.java
@@ -6,7 +6,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-public class FindCustomuerbyIdConfig {
+public class FindCustomerByIdConfig {
 
     @Bean
     public FindCustomerByIdUseCaseImpl findCustomerByIdUseCase(FindCustomerByIdImpl findCustomerById) {
