77bdb3764a35c09be12989eeeb27fe24fc2205d1
George Salu <george.salu10@gmail.com>
2025-09-16 10:15:35 -0300

    hexagonal-arch - create - criando as entidades de acesso ao mongodb

---
+++ hexagonal-arch/src/main/java/com/demo/hexagonal/adapters/out/repository/entity/CustomerEntity.java
@@ -0,0 +1,61 @@
+package com.demo.hexagonal.adapters.out.repository.entity;
+
+public class CustomerEntity {
+
+    private String id;
+    private String name;
+    private AddressEntity address;
+    private String cpf;
+    private Boolean isValidCpf;
+
+    public CustomerEntity() {
+    }
+
+    public CustomerEntity(String id, String name, AddressEntity address, String cpf, Boolean isValidCpf) {
+        this.id = id;
+        this.name = name;
+        this.address = address;
+        this.cpf = cpf;
+        this.isValidCpf = isValidCpf;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public AddressEntity getAddress() {
+        return address;
+    }
+
+    public void setAddress(AddressEntity address) {
+        this.address = address;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public Boolean getValidCpf() {
+        return isValidCpf;
+    }
+
+    public void setValidCpf(Boolean validCpf) {
+        isValidCpf = validCpf;
+    }
+}
