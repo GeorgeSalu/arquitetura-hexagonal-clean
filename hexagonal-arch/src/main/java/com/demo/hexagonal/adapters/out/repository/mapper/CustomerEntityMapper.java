e53e452739b813e50e99bffffbf5cf6a753f0b8f
George Salu <george.salu10@gmail.com>
2025-09-16 10:28:41 -0300

    hexagonal-arch - create - criando o adapter de insercao de cliente

---
+++ hexagonal-arch/src/main/java/com/demo/hexagonal/adapters/out/repository/mapper/CustomerEntityMapper.java
@@ -0,0 +1,12 @@
+package com.demo.hexagonal.adapters.out.repository.mapper;
+
+import com.demo.hexagonal.adapters.out.repository.entity.CustomerEntity;
+import com.demo.hexagonal.application.core.domain.Customer;
+import org.mapstruct.Mapper;
+
+@Mapper(componentModel = "spring")
+public interface CustomerEntityMapper {
+
+    CustomerEntity toCustomerEntity(Customer customer);
+
+}
