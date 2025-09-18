88ca6dc98f39a252b86bcf47353fccf78e5b5e9e
George Salu <george.salu10@gmail.com>
2025-09-18 17:07:32 -0300

    hexagonal-arch - criando o producer do kafka

--- hexagonal-arch/src/main/java/com/demo/hexagonal/application/core/usecase/InsertCustomerUseCase.java
+++ hexagonal-arch/src/main/java/com/demo/hexagonal/application/core/usecase/InsertCustomerUseCase.java
@@ -4,15 +4,18 @@ import com.demo.hexagonal.application.core.domain.Customer;
 import com.demo.hexagonal.application.ports.in.InsertCustomerInputPort;
 import com.demo.hexagonal.application.ports.out.FindAddressByZipCodeOutputPort;
 import com.demo.hexagonal.application.ports.out.InsertCustomerOutputPort;
+import com.demo.hexagonal.application.ports.out.SendCpfForValidationOutputPort;
 
 public class InsertCustomerUseCase implements InsertCustomerInputPort {
 
     private final FindAddressByZipCodeOutputPort findAddressByZipCodeOutputPort;
     private final InsertCustomerOutputPort insertCustomerOutputPort;
+    private final SendCpfForValidationOutputPort sendCpfForValidationOutputPort;
 
-    public InsertCustomerUseCase(FindAddressByZipCodeOutputPort findAddressByZipCodeOutputPort, InsertCustomerOutputPort insertCustomerOutputPort) {
+    public InsertCustomerUseCase(FindAddressByZipCodeOutputPort findAddressByZipCodeOutputPort, InsertCustomerOutputPort insertCustomerOutputPort, SendCpfForValidationOutputPort sendCpfForValidationOutputPort) {
         this.findAddressByZipCodeOutputPort = findAddressByZipCodeOutputPort;
         this.insertCustomerOutputPort = insertCustomerOutputPort;
+        this.sendCpfForValidationOutputPort = sendCpfForValidationOutputPort;
     }
 
     @Override
@@ -20,6 +23,7 @@ public class InsertCustomerUseCase implements InsertCustomerInputPort {
         var address = findAddressByZipCodeOutputPort.find(zipCode);
         customer.setAddress(address);
         insertCustomerOutputPort.insert(customer);
+        sendCpfForValidationOutputPort.send(customer.getCpf());
     }
 
 }
--- hexagonal-arch/src/main/java/com/demo/hexagonal/config/InsertCustomerConfig.java
+++ hexagonal-arch/src/main/java/com/demo/hexagonal/config/InsertCustomerConfig.java
@@ -2,6 +2,7 @@ package com.demo.hexagonal.config;
 
 import com.demo.hexagonal.adapters.out.FindAddressByZipCodeAdapter;
 import com.demo.hexagonal.adapters.out.InsertCustomerAdapter;
+import com.demo.hexagonal.adapters.out.SendCpfValidationAdapter;
 import com.demo.hexagonal.application.core.usecase.InsertCustomerUseCase;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -10,8 +11,11 @@ import org.springframework.context.annotation.Configuration;
 public class InsertCustomerConfig {
 
     @Bean
-    public InsertCustomerUseCase insertCustomerUseCase(FindAddressByZipCodeAdapter findAddressByZipCodeAdapter, InsertCustomerAdapter insertCustomerAdapter) {
-        return new InsertCustomerUseCase(findAddressByZipCodeAdapter, insertCustomerAdapter);
+    public InsertCustomerUseCase insertCustomerUseCase(
+            FindAddressByZipCodeAdapter findAddressByZipCodeAdapter,
+            InsertCustomerAdapter insertCustomerAdapter,
+            SendCpfValidationAdapter sendCpfValidationAdapter) {
+        return new InsertCustomerUseCase(findAddressByZipCodeAdapter, insertCustomerAdapter, sendCpfValidationAdapter);
     }
 
 }
