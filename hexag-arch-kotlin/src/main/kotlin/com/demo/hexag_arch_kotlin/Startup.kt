03ebe0d602e7375cb03382582de3ef40cb0506d9
George Salu <george.salu10@gmail.com>
2025-09-20 10:50:39 -0300

    hexag-arch-kotlin - modifica classe entripoint

--- hexag-arch-kotlin/src/main/kotlin/com/demo/hexag_arch_kotlin/Startup.kt
+++ hexag-arch-kotlin/src/main/kotlin/com/demo/hexag_arch_kotlin/Startup.kt
@@ -4,8 +4,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
-class HexagArchKotlinApplication
+class Startup
 
 fun main(args: Array<String>) {
-	runApplication<HexagArchKotlinApplication>(*args)
+	runApplication<Startup>(*args)
 }
