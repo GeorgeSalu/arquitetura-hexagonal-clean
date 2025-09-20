22c5fbea14d2fae65d8d8210fc78243bd660cca2
George Salu <george.salu10@gmail.com>
2025-09-20 10:38:19 -0300

    criando o projeto hexag-arch-kotlin

---
+++ hexag-arch-kotlin/src/main/kotlin/com/demo/hexag_arch_kotlin/HexagArchKotlinApplication.kt
@@ -0,0 +1,11 @@
+package com.demo.hexag_arch_kotlin
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class HexagArchKotlinApplication
+
+fun main(args: Array<String>) {
+	runApplication<HexagArchKotlinApplication>(*args)
+}
